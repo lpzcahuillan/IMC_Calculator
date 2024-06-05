fa93b60a4951e01abfb3826c54dfe0dbd69cce40
Hugo Luis Lopez Cahuillan <61170631+lpzcahuillan@users.noreply.github.com>
2024-06-05 16:35:40 -0400

    First Commit

---
+++ IMC_calculator/IMC_calculator/IMC_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_calculatorApp.swift
+//  IMC_calculator
+//
+//  Created by Hugo López on 05-06-24.
+//
+
+import SwiftUI
+
+@main
+struct IMC_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
